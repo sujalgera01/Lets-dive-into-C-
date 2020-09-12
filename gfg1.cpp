34ec81de9ce795d2bf4fcd98e10fa4b4b9f9c058
sujalgera01 <sujalgera01@gmail.com>
2020-09-12 23:33:07 +0530

    day 3

---
+++ gfg1.cpp
@@ -0,0 +1,23 @@
+// STACK DESIGNER
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+stack<int>_push(int arr[],int n)
+{
+   stack<int>s;
+   int i;
+   for(i=0;i<n;i++){
+       s.push(arr[i]);
+   }
+   return s;
+}
+
+void _pop(stack<int>s)
+{
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
