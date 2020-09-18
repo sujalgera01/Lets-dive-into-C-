a28d83d7fa18c84f113b96d0bf0e95fe8efab33a
sujalgera01 <sujalgera01@gmail.com>
2020-09-18 23:32:27 +0530

    day 7

--- Leet2.cpp
+++ Leet2.cpp
@@ -1,61 +1,7 @@
 // // // Best Time to Buy and Sell Stock
 
-// // // INCOMPLETE..........
-
 #include<iostream>
 using namespace std;
-
-// // void min(int arr[],int n){
-// // 	int s = arr[0];
-// // 	for(int i=0;i<n;i++){
-// // 		if(arr[i]<arr[0])
-// // 			arr[0] = arr[i];
-// // 	}
-// // }
-// // void fun(int arr[],int n){
-// // 	int g,s,s1,s2;
-// // 	if(s == arr[n-1]){
-// // 		cout<<"didnot buy";
-// // 	}
-// // 	else{
-// // 		for(int i=0;i<n;i++){
-// // 			if(arr[i] == s){
-// // 				int s1 = i;
-// // 				break;
-// // 			}
-// // 		}
-
-// // 		for(int i=0;i<n;i++){
-// // 			if(arr[i] == g){
-// // 				int s2 = i;
-// // 				break;
-// // 			}
-// // 		}
-
-// // 		if(s1 < s2)
-// // 			cout<<g-s;
-// // 		else
-// // 			cout<<"cant be purchased";
-// // 	}
-// // }
-
-// void profit(int arr[],int n){
-// 	int brr[n];
-// 	for(int i=0;i<n;i++){
-// 		brr[i] = arr[i+1]-arr[i];
-// 	}
-
-// }
-
-// void max(int brr[],int n){
-// 	int g = brr[0];
-// 	for(int i=0;i<n;i++){
-// 		if(brr[i]>brr[0])
-// 			brr[0] = brr[i];
-// 	}
-
-// 	cout<<g;
-// }
 
 void profit(int arr[],int n){
 	int p = 0;
@@ -65,18 +11,17 @@ void profit(int arr[],int n){
 		if(arr[i]<b){
 			b = arr[i];
 			s = arr[i+1];
-		}
-		if(s-b>0){
-			p = (s-b);
-		}
-	}
 
-	for(int i=0;i<n;i++){
-		if(arr[i]>s){
-			s = arr[i];
+		if(s-b>p){
+			p = (s-b);
 		}
+		}
+		else if(arr[i]>s){
+			s = arr[i];
+
 		if(s-b>0){
 			p = (s-b);
+			}		
 		}
 	}
 	cout<<p;
@@ -92,5 +37,4 @@ int main(){
 	}
 
 	profit(arr,n);
-	// fun(arr,n);
 }
