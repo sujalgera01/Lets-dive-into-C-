12e5732a51a8d7db0c7691f7f90ecab3fa0a8c0b
sujalgera01 <sujalgera01@gmail.com>
2020-10-04 20:40:12 +0530

    day 12

--- Leet9.cpp
+++ Leet9.cpp
@@ -1,28 +1,18 @@
 // K-diff Pairs in an Array
 
 #include<iostream>
-#include<bits/stdc++.h>
 using namespace std;
 
 int diffPairs(int arr[],int n,int k){
-	int a,b;
-	pair<int,int> p1;
-	pair<int,int> p2 (0,0);
+	int count = 0;
 	for(int i=0;i<n;i++){
-		a = arr[i];
 		for(int j=i+1;j<n;j++){
-			b = arr[j];
-			if(b-a == k){
-				p1.first = a;
-				p1.second = b;
-				if(p1 != p2){
-					p2 = p1;
-					cout<<p2.first<<","<<p2.second<<endl;
-				}
+			if(arr[i]-arr[j] == k || arr[j]-arr[i] == k){
+				count++;
 			}		
 		}
-		
 	}
+	cout<<count;
 }
 
 int main(){
