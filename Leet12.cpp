23934c82801fc34adf01256cf2e960ebf3410ae7
sujalgera01 <sujalgera01@gmail.com>
2020-10-08 07:00:41 +0530

    day 13

--- Leet12.cpp
+++ Leet12.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int complementbaseten(int n){
 
-	int arr[10];
+	int arr[32];
 	int i;
 	for(i=0;n>0;i++){
 		arr[i] = n%2;
 		n = n/2;
 	}
 
-	for(int j=i-1;j>0;j--){
+	for(int j=i-1;j>=0;j--){
+		if(arr[j] == 1){
+			arr[j] = 0;
+		}
+		else{
+			arr[j] = 1;
+		}
 		cout<<arr[j]<<" ";
 	}
-
 }
+
 
 int main(){
 	int num;
